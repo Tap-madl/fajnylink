e5b6192311587de43a8f9b3f19bb18c2329c6114
keysha <karolina.anna.kruk@gmail.com>
2011-01-14 00:13:31 +0100

    Start projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fajnylink::Application.config.secret_token = 'fcca9d9c555f669e7b9dc3bd84b39a1312773c90bbf6d92dc5fd3969bce33a6349a273e90b43f4a45d2af17cb6446e6be263005c5c44de5d127417fa00e57bb0'
