881a4fee1e656e995c2449e5685b5a907d08a524
Seba <sebah1989@gmail.com>
2011-01-14 02:42:17 +0100

    shebel Sebastian Hebel. Dodane usuwanie komentarzy. Tylko autor moze usuwac swoj komentarz

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 class CommentsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authorize_user_destroy, :only => [:destroy]
   def create
     @link = Link.find(params[:link_id])
     @comment = @link.comments.create(params[:comment])
@@ -7,4 +9,21 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to link_path(@link)
   end
+
+  def destroy
+    @comment.destroy
+    redirect_to link_path(@link)
+  end
+
+  protected
+
+  def authorize_user_destroy
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.find(params[:id])
+    if @comment.user != current_user
+      redirect_to link_path(@link)
+      flash[:notice] = "Brak uprawnien."
+    end
+  end
+
 end
