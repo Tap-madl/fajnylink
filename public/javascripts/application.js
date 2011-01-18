58616764d93e70bee69204192c2d02f58cc06d80
mojuk <mojuk@red7.(none)>
2011-01-18 17:59:36 +0100

    mslomka Mateusz Słomka dokończenie sortowania i wyszukiwania

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  $("#links th a, #links .pagination a").live("click", function() {
+     $.getScript(this.href);
+     return false;
+  });
+  $("#links_search input").keyup(function() {
+     $.get($("#links_search").attr("action"), $("#links_search").serialize(), null, "script");
+     return false;
+  });
+});
+
